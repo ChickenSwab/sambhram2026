c08340d7ae93b0fc5f838d14abdfbadda65d8325
ekahPruthvi <p1sribhat@gmail.com>
2026-04-01 22:19:06 +0530

    fix small issue

--- app.js
+++ app.js
@@ -91,13 +91,31 @@ const menubtn = document.getElementById("menu");
 
 menubtn.addEventListener("click", function(event) {
     event.stopPropagation();
-    menubtn.innerHTML = `
-        <ul class="bounce-animate>
-            <li class="active">Home</li>
-            <li><a href="./events.html">Events</a></li>
-            <li><a href="./contact.html">Contact Us</a></li>
-        </ul>
-    `;
+    const path = window.location.pathname;
+    const page = path.split("/").pop();
+    const isHome = (page === "index.html" || page === "");
+
+    if (isHome) {
+        console.log("homeee");
+        menubtn.innerHTML = `
+            <ul class="bounce-animate>
+                <li class="active">Home</li>
+                <li><a href="./events.html">Events</a></li>
+                <li><a href="./contact.html">Contact Us</a></li>
+            </ul>
+        `;
+    } else {
+        console.log("no homee");
+        menubtn.innerHTML = `
+            <ul class="bounce-animate>
+                <li><a href="./index.html"></a></li>
+                <li><a href="./index.html">Home</a></li>
+                <li><a href="./events.html">Events</a></li>
+                <li class="active">Contact Us</li>
+            </ul>
+        `;
+    }
+   
     menubtn.classList.add("is-open");
 });
 
